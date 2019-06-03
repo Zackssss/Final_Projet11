4d1fee7a9766635559f1873fe4a21edb187af21e
J-B <jeanbaptiste.schmitt@viacesi.fr>
2019-06-03 16:27:16 +0200

    tests pliés

---
+++ main/src/test/main/MainTest.java
@@ -0,0 +1,47 @@
+package main;
+
+import org.junit.Test;
+
+import java.io.IOException;
+import java.sql.SQLException;
+
+public class MainTest {
+
+    @Test
+    public void testMain() throws Exception {
+        // Setup
+        final String[] args = new String[]{};
+
+        // Run the test
+        Main.main(args);
+
+        // Verify the results
+    }
+
+    @Test(expected = IOException.class)
+    public void testMain_ThrowsIOException() throws Exception {
+        // Setup
+        final String[] args = new String[]{};
+
+        // Run the test
+        Main.main(args);
+    }
+
+    @Test(expected = SQLException.class)
+    public void testMain_ThrowsSQLException() throws Exception {
+        // Setup
+        final String[] args = new String[]{};
+
+        // Run the test
+        Main.main(args);
+    }
+
+    @Test(expected = InterruptedException.class)
+    public void testMain_ThrowsInterruptedException() throws Exception {
+        // Setup
+        final String[] args = new String[]{};
+
+        // Run the test
+        Main.main(args);
+    }
+}
---
+++ model/src/test/java/model/ModelTest.java
@@ -0,0 +1,109 @@
+package model;
+
+import contract.ControllerOrder;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.sql.SQLException;
+import java.util.Observable;
+
+import static org.junit.Assert.assertArrayEquals;
+import static org.junit.Assert.assertEquals;
+
+public class ModelTest {
+
+    private Model modelUnderTest;
+
+    @Before
+    public void setUp() throws SQLException {
+        modelUnderTest = new Model();
+    }
+
+    @Test
+    public void testGetInfos() throws Exception {
+        // Setup
+        final int expectedResult = 10;
+
+        // Run the test
+        final int result = modelUnderTest.getInfos();
+
+        // Verify the results
+        assertEquals(expectedResult, result);
+    }
+
+
+
+    @Test(expected = SQLException.class)
+    public void testGetSize_ThrowsSQLException() throws Exception {
+        // Setup
+
+        // Run the test
+        modelUnderTest.getSize();
+    }
+
+    @Test
+    public void testSetMap() throws Exception {
+        // Setup
+        final int ID = 0;
+
+        // Run the test
+        modelUnderTest.setMap(ID);
+
+        // Verify the results
+    }
+
+
+
+
+    @Test
+    public void testIsFalling() {
+        // Setup
+
+        // Run the test
+        modelUnderTest.isFalling();
+
+        // Verify the results
+    }
+
+    @Test
+    public void testMonsterOrder() {
+        // Setup
+
+        // Run the test
+        modelUnderTest.monsterOrder();
+
+        // Verify the results
+    }
+
+    @Test
+    public void testSlip() {
+        // Setup
+
+        // Run the test
+        modelUnderTest.slip();
+
+        // Verify the results
+    }
+
+    @Test
+    public void testDeath() throws Exception {
+        // Setup
+
+        // Run the test
+        modelUnderTest.death();
+
+        // Verify the results
+    }
+
+
+    @Test
+    public void testWin() throws Exception {
+        // Setup
+
+        // Run the test
+        modelUnderTest.win();
+
+        // Verify the results
+    }
+
+}
