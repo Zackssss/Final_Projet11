0087023f80350690a262860032ac16fce68df143
Grivznof <pbaillet9@gmail.com>
2019-06-03 10:58:50 +0200

    pl add folder test

---
+++ model/src/test/java/ModelTest.java
@@ -0,0 +1,13 @@
+import junit.framework.TestCase;
+
+public class ModelTest extends TestCase {
+
+    public void testGetInfos() {
+    }
+
+    public void testSetID() {
+    }
+
+    public void testGetID() {
+    }
+}
